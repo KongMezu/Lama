921a8175493ed8ff91f0c9220bdaba8f8fc2378a
unknown <sumin50240585@gmail.com>
2024-05-21 02:23:06 +0900

    과제4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+
+function Calculator() {
+  const [count, setCount] = useState(1);
+
+  const handleMultiply = () => {
+    setCount(prevCount => prevCount * 2);
+  };
+
+  const handleDivide = () => {
+    setCount(prevCount => prevCount / 2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleMultiply}>곱하기</button>
+      <button onClick={handleDivide}>나누기</button>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
+
+// function App() {
+//   const [isVisible, setIsVisible] = useState(true);
+
+//   const toggleVisibility = () => {
+//     setIsVisible(!isVisible);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={toggleVisibility}>DEL</button>
+//       {isVisible && <img src="logo192.png" />}
+//     </div>
+//   );
+// }
+
+// export default App;
